f5b6a67dbb7de9947776d5d63aa5c7881b17c2a6
Saifallah AZZABI <sazzabi@figarocms.fr>
2022-03-16 21:43:05 +0100

    add stimulus

--- assets/controllers/farm_controller.js
+++ assets/controllers/farm_controller.js
@@ -1,7 +1,15 @@
+// assets/controllers/farm_controller.js
 import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
-    connect() {
+    static values = {
+        animal: String
+    }
+    static targets = ['barn'];
 
+    grow(event) {
+        event.preventDefault();
+        this.barnTarget.innerHTML = this.barnTarget.innerHTML
+            + this.animalValue;
     }
 }
