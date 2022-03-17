5e23dc425618dcbb13125d68d03d3afed8abbf8c
Saifallah AZZABI <sazzabi@figarocms.fr>
2022-03-17 09:41:57 +0100

    final: plain old javascript inside stimulus controller

--- assets/controllers/farm_controller.js
+++ assets/controllers/farm_controller.js
@@ -2,6 +2,12 @@ import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
     connect() {
+        const barnEl = document.getElementById('barn');
+        const growBarnButton = document.querySelector('.js-grow-barn');
+        growBarnButton.addEventListener('click', (event) => {
+            event.preventDefault();
 
+            barnEl.innerHTML = barnEl.innerHTML + barnEl.dataset.animal;
+        });
     }
 }
